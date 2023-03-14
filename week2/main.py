dbf31d8c98709ef99ea1472c5888c8f68a4ae858
0Miquel <longi297>
2023-03-14 15:16:35 +0100

    custom kitti dataset in dataset.py

--- week2/main.py
+++ week2/main.py
@@ -4,6 +4,8 @@ from detectron2.utils.visualizer import Visualizer
 import matplotlib.pyplot as plt
 import cv2
 import random
+from detectron2.engine import DefaultPredictor
+from config import custom_config
 
 
 for d in ["train"]:
@@ -12,11 +14,25 @@ for d in ["train"]:
 
 kitti_metadata = MetadataCatalog.get("kitti_train")
 
+cfg = custom_config()
+
+predictor = DefaultPredictor(cfg)
+
 dataset_dicts = get_kitti_dataset("../dataset/KITTI-MOTS/", "train")
+
 for d in random.sample(dataset_dicts, 5):
-    img = cv2.imread(d["file_name"])
-    visualizer = Visualizer(img[:, :, ::-1], metadata=kitti_metadata, scale=0.5)
-    out = visualizer.draw_dataset_dict(d)
+    im = cv2.imread(d["file_name"])
+    outputs = predictor(im)
+    visualizer = Visualizer(im[:, :, ::-1], metadata=kitti_metadata, scale=0.5)
+
+    out_pred = visualizer.draw_instance_predictions(outputs["instances"].to("cpu"))
     plt.figure(figsize=(15, 7))
-    plt.imshow(out.get_image()[:, :, ::-1][..., ::-1])
+    plt.imshow(out_pred.get_image()[:, :, ::-1][..., ::-1])
+    plt.title("Prediction")
+    plt.show()
+
+    out_gt = visualizer.draw_dataset_dict(d)
+    plt.figure(figsize=(15, 7))
+    plt.imshow(out_gt.get_image()[:, :, ::-1][..., ::-1])
+    plt.title("Ground truth")
     plt.show()
