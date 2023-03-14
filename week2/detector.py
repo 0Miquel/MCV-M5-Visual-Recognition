436333ecbbabe6de058af0c7e585eff1621acff5
0Miquel <longi297>
2023-03-14 09:52:15 +0100

    code organized in weeks

---
+++ week2/detector.py
@@ -0,0 +1,33 @@
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+import cv2
+import numpy as np
+
+
+class Detector:
+    def __init__(self, model_cfg):
+        self.cfg = get_cfg()
+
+        self.cfg.merge_from_file(model_zoo.get_config_file(model_cfg))
+        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_cfg)
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+        self.cfg.MODEL.DEVICE = "cuda"
+
+        self.predictor = DefaultPredictor(self.cfg)
+
+    def inference(self, im_path):
+        im = cv2.imread(im_path)[:, :, ::-1]
+        v = Visualizer(im, MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), scale=1.2)
+
+        outputs = self.predictor(im)
+        out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+        cv2.imshow("Result", out.get_image()[..., ::-1])
+        cv2.waitKey(0)
--- week2/main.py
+++ week2/main.py
@@ -1,39 +1,5 @@
-import torch
-import torchvision
-
-import detectron2
-from detectron2.utils.logger import setup_logger
-setup_logger()
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2.structures import BoxMode
-
-import numpy as np
-import os, json, cv2, random
-import matplotlib.pyplot as plt
+from detector import Detector
 
 
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-
-predictor = DefaultPredictor(cfg)
-
-im = cv2.imread("./input.jpg")[..., ::-1]
-print(im.shape)
-plt.figure(figsize=(15,7.5))
-plt.imshow(im)
-plt.show()
-
-outputs = predictor(im)
-
-v = Visualizer(im, MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-plt.figure(figsize=(20,10))
-plt.imshow(out.get_image()[..., ::-1])
-plt.show()
-
+detector = Detector("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+detector.inference("./input.jpg")
