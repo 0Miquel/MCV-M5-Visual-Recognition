94e9476612d4740b0bfd6420c59874d96f133355
0Miquel <longi297>
2023-03-22 13:00:13 +0100

    task a and b

---
+++ week3/task_a.py
@@ -0,0 +1,62 @@
+import os
+
+from dataset import create_detectron_dataset
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.utils.visualizer import Visualizer
+import matplotlib.pyplot as plt
+import cv2
+import random
+import torch
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import build_detection_test_loader
+
+import argparse
+import sys
+from utils import *
+from datetime import datetime
+
+
+def main():
+    now = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")+"_task_a"
+    os.makedirs('../results', exist_ok=True)
+    os.makedirs(f'../results/{now}', exist_ok=True)
+    paths = [f'../results/{now}/faster', f'../results/{now}/mask']
+    models = ["COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml",
+              "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"]
+    dataset_path = "../dataset/out_of_context/"
+
+    for path, model in zip(paths, models):
+        # Create Predictor
+        cfg = get_cfg()
+        cfg.merge_from_file(model_zoo.get_config_file(model))
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model)
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # confidence threshold
+        cfg.MODEL.DEVICE = "cuda"
+        predictor = DefaultPredictor(cfg)
+
+        dataset_dicts = create_detectron_dataset(dataset_path)
+        random.seed(42)
+        for i, sample in enumerate(random.sample(dataset_dicts, 10)):
+            img = cv2.imread(sample["file_name"])
+            # Predictions
+            predictions = predictor(img)
+            visualizer = Visualizer(img[:, :, ::-1],MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=0.5)
+            out = visualizer.draw_instance_predictions(predictions["instances"].to("cpu"))
+            outimgpred = out.get_image()[:, :, ::-1]
+
+            os.makedirs(path, exist_ok=True)
+            cv2.imwrite(f"{path}/pred{str(i).zfill(3)}.jpg", outimgpred)
+
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--config", default="config/inference_and_eval.yaml")
+    # args = parser.parse_args(sys.argv[1:])
+    #
+    # config_path = args.config
+    #
+    # config = load_yaml_config(config_path)
+    main()
---
+++ week3/task_b.py
@@ -0,0 +1,95 @@
+from pycocotools.coco import COCO
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.data import MetadataCatalog
+from detectron2.utils.visualizer import Visualizer
+
+
+###############################################################
+# PARAMETERS YOU CAN CHANGE
+###############################################################
+# source image where you extract the object
+object_img_path = "../dataset/coco2017/val2017/000000020247.jpg"
+# object id we want to extract and paste
+object_id = 23  # check COCO objects id in https://github.com/pjreddie/darknet/blob/master/data/coco.names
+# number of object to extract in case there are more than one of the same category in the image
+object_idx = 0
+# destination image where you paste the object
+source_img_path = "../dataset/coco2017/val2017/000000000139.jpg"
+# specify the path to the annotation file
+annFile = '../dataset/coco2017/annotations/instances_val2017.json'
+# position of pasted object in destination image
+position = (-100, -100)
+
+
+###############################################################
+# GET OBJECT MASK USING COCO API
+###############################################################
+# initialize the COCO API
+coco = COCO(annFile)
+# specify the ID of the image
+img_id = int(object_img_path.split("/")[-1].split(".")[0])
+# get the information for the image
+img_info = coco.loadImgs(img_id)[0]
+# get the annotations for the image
+ann_ids = coco.getAnnIds(imgIds=img_id)
+anns = coco.loadAnns(ann_ids)
+# get the segmentation mask for the object in the image
+object_anns = [ann for ann in anns if ann["category_id"] == object_id]
+if not object_anns:
+    raise f"No object with id {object_id} found in this image"
+elif len(object_anns) > 1:
+    print(f"More than one object with id {object_id} found, extracting the first one found")
+mask = coco.annToMask(object_anns[object_idx])
+
+
+###############################################################
+# CREATE RESULTING IMAGE
+###############################################################
+# get the object image
+object_img = Image.open(object_img_path)
+plt.imshow(np.array(object_img))
+plt.show()
+# get the destination image
+source_img = Image.open(source_img_path)
+plt.imshow(np.array(source_img))
+plt.show()
+# transform to mask to PIL
+mask = Image.fromarray(mask*255)
+# Apply the mask to the object image
+object_masked_img = object_img.convert("RGBA")
+object_masked_img.putalpha(mask)
+source_img.paste(object_masked_img, position, object_masked_img)
+# # save image
+# source_img.save("new.png", format="png")
+# plot resulting image
+result_img = np.array(source_img)
+plt.imshow(result_img)
+plt.show()
+
+
+###############################################################
+# INFERE RESULTING IMAGE WITH FASTER AND MASK MODELS
+###############################################################
+models = ["COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml",
+          "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"]
+for model in models:
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(model))
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model)
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # confidence threshold
+    cfg.MODEL.DEVICE = "cuda"
+    predictor = DefaultPredictor(cfg)
+
+    predictions = predictor(result_img)
+    visualizer = Visualizer(result_img[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=0.5)
+    out = visualizer.draw_instance_predictions(predictions["instances"].to("cpu"))
+    outimgpred = out.get_image()[:, :, ::-1]
+
+    plt.imshow(outimgpred)
+    plt.tight_layout()
+    plt.show()
